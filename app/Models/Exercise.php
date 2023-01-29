b48269452b656eb0cee866ef3d7b917994f08eb6
Jorge Villegas <jorge.villegasp@hotmail.com>
2023-01-29 12:14:29 -0500

    MODEL: Creando las relacion entre Serie y Exercise

---
+++ app/Models/Exercise.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Exercise extends Model
+{
+    use HasFactory;
+
+    /**
+     * Esta función establece una relación entre la clase
+     * actual (Exercise) y la clase Routine
+     *
+     * Relacion de uno a muchos (invertido)
+     *
+     */
+    public function routine(){
+        return $this->belongsTo(Routine::class);
+    }
+
+}
